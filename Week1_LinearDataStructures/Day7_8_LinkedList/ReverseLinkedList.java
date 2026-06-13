ca91fb3c0653c80df8e264a9b3331de6719d43b8
HARI PAVAN INGILALA <161825246+Hari8328@users.noreply.github.com>
2026-06-13 17:16:50 +0530

    Add ReverseLinkedList class with reversal method
    Implement a linked list reversal and print functionality.

---
+++ Week1_LinearDataStructures/Day7_8_LinkedList/ReverseLinkedList.java
@@ -0,0 +1,53 @@
+import java.util.*;
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class ReverseLinkedList {
+
+    public static ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+
+            curr.next = prev;
+
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        head = reverse(head);
+
+        System.out.println("Reversed List:");
+        printList(head);
+    }
+}
