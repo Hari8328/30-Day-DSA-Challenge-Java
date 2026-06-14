740d04065b71d1c43e85a17d3642f4f501f17d4b
HARI PAVAN INGILALA <161825246+Hari8328@users.noreply.github.com>
2026-06-14 11:06:31 +0530

    Implement Remove Nth Node from End of List

---
+++ Week1_LinearDataStructures/Day7_8_LinkedList/RemoveNthNode.java
@@ -0,0 +1,55 @@
+import java.util.*;
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class RemoveNthNode {
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+
+    public static void print(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        head = removeNthFromEnd(head, 2);
+
+        print(head);
+    }
+}
