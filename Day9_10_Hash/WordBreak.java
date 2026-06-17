2b47080aa2ff4290dc86a681040dfeef06c4e93b
HARI PAVAN INGILALA <161825246+Hari8328@users.noreply.github.com>
2026-06-17 12:00:55 +0530

    Update WordBreak.java

---
+++ Day9_10_Hash/WordBreak.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class WordBreak {
+
+    public static boolean wordBreak(
+            String s,
+            List<String> wordDict) {
+
+        Set<String> dict =
+                new HashSet<>(wordDict);
+
+        boolean[] dp =
+                new boolean[s.length() + 1];
+
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                if (dp[j]
+                        && dict.contains(
+                        s.substring(j, i))) {
+
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                wordBreak(
+                        "leetcode",
+                        Arrays.asList(
+                                "leet",
+                                "code"
+                        )
+                )
+        );
+
+        System.out.println(
+                wordBreak(
+                        "catsandog",
+                        Arrays.asList(
+                                "cat",
+                                "cats",
+                                "and",
+                                "sand",
+                                "dog"
+                        )
+                )
+        );
+    }
+}
