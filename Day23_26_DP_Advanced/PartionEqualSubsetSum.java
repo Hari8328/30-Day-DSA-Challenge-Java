0a34bcc36f343f79eb360f8ac56b0f3640dac666
HARI PAVAN INGILALA <161825246+Hari8328@users.noreply.github.com>
2026-06-30 20:50:21 +0530

    Implement Partition Equal Subset Sum algorithm

---
+++ Day23_26_DP_Advanced/PartionEqualSubsetSum.java
@@ -0,0 +1,55 @@
+import java.util.*;
+public class PartitionEqualSubsetSum {
+
+    public static boolean canPartition(
+            int[] nums) {
+
+        int sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+        }
+
+        if (sum % 2 != 0) {
+            return false;
+        }
+
+        int target = sum / 2;
+
+        boolean[] dp =
+                new boolean[target + 1];
+
+        dp[0] = true;
+
+        for (int num : nums) {
+
+            for (int i = target;
+                 i >= num;
+                 i--) {
+
+                dp[i] =
+                        dp[i] ||
+                        dp[i - num];
+            }
+        }
+
+        return dp[target];
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums1 =
+                {1, 5, 11, 5};
+
+        int[] nums2 =
+                {1, 2, 3, 5};
+
+        System.out.println(
+                canPartition(nums1)
+        );
+
+        System.out.println(
+                canPartition(nums2)
+        );
+    }
+}
